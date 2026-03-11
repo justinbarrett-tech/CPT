22521421e92b23b9e9d68c5d23163e5a03faca8b
Justin Barrett <justin.barrett@crpusd.org>
2026-03-11 09:21:25 -0700

    Added comments, got rid of some unnecessary things

--- CPT.py
+++ CPT.py
@@ -1,29 +1,30 @@
 # All the account stuff
-
+# Lists for accounts and scores tied to those accounts by index
 accounts = ["ApplePie23","StrayMango38","GrayOwl57","Dxnte_707","sizzle","JB"]
 scores = [5, 9, 6, 5, 11, 5]
+
+# Gets user_account
 user_acnt = input("What is your username? If you do not have one, what would you like it to be? \n")
 
-def check_acnt(account):
- if account in accounts:
-  print("We found your account!")
- else:
-  print("We are adding your account now!")
-  accounts.append(account)
-  scores.append(0)
+# Checks if user_acnt in list of accounts and adds it if not
+if user_acnt in accounts:
+ print("We found your account!")
+else:
+ print("We are adding your account now!")
+ accounts.append(user_acnt)
+ scores.append(0)
 
-check_acnt(user_acnt)
-
+# Asks the user what color it wants j to be
+# to be used later on
 user_color = input("What color would you like the moving turtle to be? \n")
 
 # All the game stuff
+# Importing things
 import turtle as t
 import random as r
-import time as tm
 
-nth = 0
-
-
+# Define turtles and add characteristics
+# Moving/Clicking turtle
 j = t.Turtle()
 j.hideturtle()
 j.penup()
@@ -32,6 +33,7 @@ j.left(90)
 j.shapesize(4)
 j.speed(1)
 
+# Start button turtle
 st = t.Turtle()
 st.hideturtle()
 st.shape("square")
@@ -39,23 +41,29 @@ st.shapesize(5)
 st.color("green")
 st.showturtle()
 
+# Turtle for writing start
 w = t.Turtle()
 w.hideturtle()
 w.penup()
 w.goto(-30,50)
 w.write("START", font="Arial")
 
+# Turtle for writing the top words
 w2 = t.Turtle()
 w2.hideturtle()
 w2.penup()
 w2.goto(-155,300)
 
+# Gets a point counter set up
 count = 0
 
+# Add function to keep track of points
 def add(x,y):
  global count
  count = count + 1
 
+# Function for running the game so that 
+# after the button is pressed the game runs
 def run_game(color,nth):
  j.color(color)
  w.clear()
@@ -82,7 +90,14 @@ def run_game(color,nth):
  else:
   print("You did not reach a new highscore. Your highscore is", scores[ind])
 
+# Helps give another arguement to ensure 
+# the code runs when clicked
+nth = 0
+
+# Where it calls all the functions
+# Adds to points for each click
 j.onclick(add)
+# Starts game only after st is pressed
 st.onclick(lambda x,y: run_game(user_color,nth))
 wn = t.Screen()
 wn.mainloop()
